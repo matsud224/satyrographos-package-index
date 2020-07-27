37e111dbfb4d1cadbbd9e1d5f08ee4c529735030
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-07-27 16:34:23 +0900

    refactor

--- pkgjson.ml
+++ pkgjson.ml
@@ -1,33 +1,60 @@
-let package_dir_name = "satyrographos-repo/packages"
-let doc_root_dir_name = "./docs"
+(*
+ * before running this program...
+ *   1. Clone 'satyrographos-repo' in current directory.
+ *   2. Install 'satyrographos-snapshot-stable' via opam.
+ *   3. Run 'satyrographos install'.
+ *   4. Copy '.satysfi/dist/docs' in current directory.
+ *)
 
-type package_type = Library | Class | Font | Document | Satysfi | Satyrographos | Other
+let package_root = "./satyrographos-repo/packages"
+let doc_root = "./docs"
+let out_file = "./data.json"
 
-type package = {
-  name : string;
-  package_type : package_type;
+type package_type =
+  Library | Class | Font | Document | Satysfi | Satyrographos | Other
+
+type package_info = {
+  name           : string;
+  pkg_type       : package_type;
+  synopsis       : string;
+  description    : string;
+  maintainer     : string;
+  license        : string;
+  homepage       : string;
+  latest_version : string;
+  dependencies   : string;
+  documents      : string list;
 }
 
+let string_of_package_type t =
+  match t with
+  | Library       -> "Library"
+  | Class         -> "Class"
+  | Font          -> "Font"
+  | Document      -> "Document"
+  | Satysfi       -> "Satysfi"
+  | Satyrographos -> "Satyrographos"
+  | Other         -> "Other"
 
-let remove_head str n = String.sub str n ((String.length str) - n)
+let remove_head n str = String.sub str n ((String.length str) - n)
 
 let get_package_list () =
-  Sys.readdir package_dir_name |> Array.to_list
+  Sys.readdir package_root |> Array.to_list
 
 let get_version_list name =
-  let prefix_len = (String.length name) + 1 in
-  let dirs = Sys.readdir (package_dir_name ^ "/" ^ name) |> Array.to_list in
-  List.map (fun str -> remove_head str prefix_len) dirs |> List.sort (fun a b -> -(String.compare a b))
+  let pkgname_part_len = String.length (name ^ ".") in
+  let dirs = Sys.readdir (Filename.concat package_root name) |> Array.to_list in
+  List.map (remove_head pkgname_part_len) dirs |> List.sort (fun a b -> String.compare b a)
 
 let get_package_type name =
   let open Str in
   let regexp_type_pair = [
-    (regexp ".*-doc$", Document);
+    (regexp ".*-doc$",           Document);
     (regexp "^satysfi-class-.*", Class);
     (regexp "^satysfi-fonts-.*", Font);
-    (regexp "^satysfi$", Satysfi);
-    (regexp "^satyrographos.*", Satyrographos);
-    (regexp "^satysfi-.*", Library)
+    (regexp "^satysfi$",         Satysfi);
+    (regexp "^satyrographos.*",  Satyrographos);
+    (regexp "^satysfi-.*",       Library)
   ] in
   let rec iter lst =
     match lst with
@@ -39,16 +66,6 @@ let get_package_type name =
           iter rest
   in
   iter regexp_type_pair
-
-let string_of_package_type t =
-  match t with
-  | Library       -> "Library"
-  | Class         -> "Class"
-  | Font          -> "Font"
-  | Document      -> "Document"
-  | Satysfi       -> "Satysfi"
-  | Satyrographos -> "Satyrographos"
-  | Other         -> "Other"
 
 let find_variable_in_opamfile ofile name =
   let open OpamParserTypes in
@@ -64,45 +81,32 @@ let find_variable_in_opamfile ofile name =
   in
   iter ofile.file_contents
 
-let find_string_variable_in_opamfile ofile name default =
+let find_string_variable_in_opamfile ofile name =
   match find_variable_in_opamfile ofile name with
-  | Some(String(_, strval)) -> strval
-  | _ -> default
+  | Some(String(_, strval)) -> Some(strval)
+  | _                       -> None
 
-let find_deps_variable_in_opamfile ofile name default =
-  match find_variable_in_opamfile ofile name with
-  | Some(List(_, vallst)) -> String.concat ", " (List.map OpamPrinter.value vallst)
-  | _ -> default
-
-type package_info = {
-  name : string;
-  pkg_type : package_type;
-  synopsis : string;
-  description : string;
-  maintainer : string;
-  license : string;
-  homepage : string;
-  latest_version : string;
-  dependencies : string;
-  document : string list;
-}
+let get_depends_info_in_opamfile ofile =
+  match find_variable_in_opamfile ofile "depends" with
+  | Some(List(_, vallst)) -> Some(String.concat ", " (List.map OpamPrinter.value vallst))
+  | _                     -> None
 
 let json_of_package_info info =
   `Assoc [
-    ("name", `String info.name);
-    ("type", `String (string_of_package_type info.pkg_type));
-    ("synopsis", `String info.synopsis);
-    ("description", `String info.description);
-    ("maintainer", `String info.maintainer);
-    ("license", `String info.license);
-    ("homepage", `String info.homepage);
+    ("name",           `String info.name);
+    ("type",           `String (string_of_package_type info.pkg_type));
+    ("synopsis",       `String info.synopsis);
+    ("description",    `String info.description);
+    ("maintainer",     `String info.maintainer);
+    ("license",        `String info.license);
+    ("homepage",       `String info.homepage);
     ("latest_version", `String info.latest_version);
-    ("dependencies", `String info.dependencies);
-    ("document", `List (List.map (fun s -> `String s) info.document));
+    ("dependencies",   `String info.dependencies);
+    ("document",        `List (List.map (fun s -> `String s) info.documents));
   ]
 
-let json_of_package_info_list lst =
-  `Assoc [("data", `List lst)]
+let json_of_package_info_list ilst =
+  `Assoc [("data", `List (List.map json_of_package_info ilst))]
 
 let dir_is_empty dir =
   Array.length (Sys.readdir dir) = 0
@@ -124,7 +128,7 @@ let get_docfile_list name =
   let open Str in
   if string_match (regexp "^satysfi-\\(.*-doc$\\)") name 0 then
     let doc_dir_name = matched_group 1 name in
-    let doc_path = doc_root_dir_name ^ "/" ^ doc_dir_name in
+    let doc_path = doc_root ^ "/" ^ doc_dir_name in
     if Sys.file_exists doc_path && Sys.is_directory doc_path then
       dir_contents doc_path
     else
@@ -132,37 +136,28 @@ let get_docfile_list name =
   else
     []
 
-let run_command cmd =
-  if Sys.command cmd != 0 then
-    failwith "command \"%s\" failed!\n"
-  else
-    ()
-
 let () =
-  run_command "rm -rf ./docs";
-  run_command "cp -rL ~/.satysfi/dist/docs .";
   let package_list = get_package_list () in
-  let package_info_list = List.map (fun name ->
+  let package_info_list = package_list |> List.map (fun name ->
     let version_list = get_version_list name in
     let latest_version = List.hd version_list in
-    let ofile_path = package_dir_name ^ "/" ^ name ^ "/" ^ name ^ "." ^ latest_version ^ "/opam" in
-    let ofile = OpamParser.file ofile_path in
-    let str_variable = find_string_variable_in_opamfile ofile in
+    let opamfile_path = List.fold_left Filename.concat package_root [name; name ^ "." ^ latest_version; "opam"] in
+    let ofile = OpamParser.file opamfile_path in
+    let get_str_variable nm = Option.value (find_string_variable_in_opamfile ofile nm) ~default:"" in
     {
       name = name;
-      pkg_type = get_package_type name;
-      synopsis = str_variable "synopsis" "";
-      description = str_variable "description" "";
-      maintainer = str_variable "maintainer" "";
-      license = str_variable "license" "";
-      homepage = str_variable "homepage" "";
+      pkg_type       = get_package_type name;
+      synopsis       = get_str_variable "synopsis";
+      description    = get_str_variable "description";
+      maintainer     = get_str_variable "maintainer";
+      license        = get_str_variable "license";
+      homepage       = get_str_variable "homepage";
       latest_version = latest_version;
-      dependencies = find_deps_variable_in_opamfile ofile "depends" "(no dependencies)";
-      document = get_docfile_list name;
-    }) package_list
+      dependencies   = Option.value (get_depends_info_in_opamfile ofile) ~default:"(no dependencies)";
+      documents      = get_docfile_list name;
+    })
   in
-  let json_all = json_of_package_info_list (List.map json_of_package_info package_info_list) in
-  let outfile = "data.json" in
-  let ochan = open_out outfile in
-  Yojson.Basic.pretty_to_channel ochan json_all;
+  let json_root = json_of_package_info_list package_info_list in
+  let ochan = open_out out_file in
+  Yojson.Basic.pretty_to_channel ochan json_root;
   close_out ochan
