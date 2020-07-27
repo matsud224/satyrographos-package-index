4db1ea22385f11358fda9bd9098d9638a14dec6a
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-07-27 22:14:59 +0900

    add update checker

---
+++ update-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+diff data.json data_new.json
+
+if [ $? = 0 ]; then
+  git checkout .
+  echo "no updates found"
+else
+  mv data_new.json data.json
+  git config user.name github-actions
+  git config user.email github-actions@github.com
+  git add data.json
+  git add docs/*
+  git commit -m "update package data"
+  git push
+fi
