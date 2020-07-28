12e430b33fe9467008cce32a1e51dd328c24c6fa
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-07-28 12:32:53 +0900

    fix install command

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function emitDetailsTable(d) {
         <td>Installation:</td>
         <td>
         <div class="input-group mb-3">
-          <input type="text" class="form-control" aria-label="Installation Command" id="installcmd-${escaped}" value="opam install ${escaped}" readonly>
+          <input type="text" class="form-control" aria-label="Installation Command" id="installcmd-${escaped}" value="opam install ${escaped} && saryrographos install" readonly>
           <div class="input-group-append">
             <button class="btn btn-outline-secondary" type="button" data-clipboard-target="#installcmd-${escaped}">Copy</button>
           </div>
