ee1575b04f4136c64030c7bb576d80c28101a38d
Daiki Matsunaga <matsu.d.224@icloud.com>
2020-09-28 17:10:00 +0900

    fix text of datatables

--- main.js
+++ main.js
@@ -179,6 +179,14 @@ $(document).ready(function() {
   var table = $('#main-table').DataTable({
     lengthMenu: [ [50, 100, -1], [50, 100, "All"] ],
     ajax: "./data.json",
+    language: {
+      "info":           "Showing _START_ to _END_ of _TOTAL_ packages",
+      "infoEmpty":      "Showing 0 to 0 of 0 packages",
+      "infoFiltered":   "(filtered from _MAX_ total packages)",
+      "lengthMenu":     "Show _MENU_ packages",
+      "search":         "Search:",
+      "zeroRecords":    "No matching packages found"
+    },
     columns: [
       {
         className: "details-control",
